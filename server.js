fd49723c3cc276e6d2cee8e63ed08028f644224d
kinghon <kinghon@users.noreply.github.com>
2026-08-21 19:11:11 -0700

    security: allow same-origin iframes (SAMEORIGIN vs DENY)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // ===== SECURITY HEADERS (Helmet-style) =====
 app.disable('x-powered-by');
 app.use((req, res, next) => {
-  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   next();
